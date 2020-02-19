c8a651399e6f892ed1eef765a92468a12e424eea
BenjaminPcrd <benjamin.picard0@gmail.com>
2020-02-19 22:33:54 +0000

    export to csv button

---
+++ build/precache-manifest.ef4579dbce1c1e2616ebcdef09ee40d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "601c21f4cee0ddc26944e40db22578b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63445f9b7b8bf57df4b7",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "63445f9b7b8bf57df4b7",
+    "url": "/static/js/2.6b219c3a.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/static/js/2.6b219c3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4d21955153b9d1ff85b",
+    "url": "/static/js/main.33195296.chunk.js"
+  },
+  {
+    "revision": "da2128718a31f740a76e",
+    "url": "/static/js/runtime-main.04cfb6d1.js"
+  },
+  {
+    "revision": "51a2cc64aea1134a7115a4ca121d415b",
+    "url": "/static/media/attendr-logo-primary.51a2cc64.png"
+  },
+  {
+    "revision": "6e03a1a3793558905d792d02433717c8",
+    "url": "/static/media/rguAccelerator.6e03a1a3.jpg"
+  }
+]);
